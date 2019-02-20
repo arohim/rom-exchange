9cbcd0bfc8cdd5c46edceac58935735090706f66
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2019-02-20 16:25:14 +0700

    clean up

--- remote/src/main/java/kim/rom/exchange/remote/ROMExchangeRemoteImpl.kt
+++ remote/src/main/java/kim/rom/exchange/remote/ROMExchangeRemoteImpl.kt
@@ -2,16 +2,10 @@ package kim.rom.exchange.remote
 
 import io.reactivex.Single
 import kim.rom.exchange.data.model.rom.ItemEntity
-import kim.rom.exchange.data.repository.BufferooRemote
 import kim.rom.exchange.data.repository.ROMExchangeRemote
 import kim.rom.exchange.remote.mapper.rom.ItemEntityMapper
 import javax.inject.Inject
 
-/**
- * Remote implementation for retrieving Bufferoo instances. This class implements the
- * [BufferooRemote] from the Data layer as it is that layers responsibility for defining the
- * operations in which data store implementation layers can carry out.
- */
 class ROMExchangeRemoteImpl @Inject constructor(private val romExchangeService: ROMExchangeService,
                                                 private val entityMapper: ItemEntityMapper) : ROMExchangeRemote {
 
