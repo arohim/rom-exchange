d86c3dc376b9ce4401bac828b35684af1d50aef5
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2019-02-20 18:19:19 +0700

    clean up

--- data/src/main/java/kim/rom/exchange/data/repository/ROMExchangeRemote.kt
+++ data/src/main/java/kim/rom/exchange/data/repository/ROMExchangeRemote.kt
@@ -1,9 +1,10 @@
 package kim.rom.exchange.data.repository
 
 import io.reactivex.Single
+import kim.rom.exchange.data.model.rom.ItemEntity
 import kim.rom.exchange.domain.model.rom.Item
 
 interface ROMExchangeRemote {
 
-    fun getItems(kw: String, exact: Boolean, type: Int, sort: String, sortDir: String, page: Int): Single<List<Item>>
+    fun getItems(kw: String, exact: Boolean, type: Int, sort: String, sortDir: String, page: Int): Single<List<ItemEntity>>
 }
