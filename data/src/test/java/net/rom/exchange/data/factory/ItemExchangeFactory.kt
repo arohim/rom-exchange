a05d1e584a274becd95a8dfcfe4e69a39960b530
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2019-02-24 15:12:06 +0700

    preparing for Item Exchange factory for unit testing

--- data/src/test/java/net/rom/exchange/data/factory/ItemExchangeFactory.kt
+++ data/src/test/java/net/rom/exchange/data/factory/ItemExchangeFactory.kt
@@ -1,8 +1,11 @@
 package net.rom.exchange.data.factory
 
-import net.rom.exchange.data.model.BufferooEntity
+import net.rom.exchange.data.factory.DataFactory.Factory.randomBoolean
+import net.rom.exchange.data.factory.DataFactory.Factory.randomDouble
+import net.rom.exchange.data.factory.DataFactory.Factory.randomInt
 import net.rom.exchange.data.factory.DataFactory.Factory.randomUuid
-import net.rom.exchange.domain.model.Bufferoo
+import net.rom.exchange.data.model.rom.*
+import net.rom.exchange.domain.model.rom.*
 
 /**
  * Factory class for Bufferoo related instances
@@ -11,28 +14,137 @@ class ItemExchangeFactory {
 
     companion object Factory {
 
-        fun makeBufferooEntity(): BufferooEntity {
-            return BufferooEntity(randomUuid(), randomUuid(), randomUuid())
+        fun makeDataItemEntity(): DataItemEntity {
+            return DataItemEntity(
+                    price = randomInt(),
+                    time = randomUuid(),
+                    snap = randomBoolean()
+            )
         }
 
-        fun makeBufferoo(): Bufferoo {
-            return Bufferoo(randomUuid(), randomUuid(), randomUuid())
+        fun makeDataItem(): DataItem {
+            return DataItem(
+                    price = randomInt(),
+                    time = randomUuid(),
+                    snap = randomBoolean()
+            )
         }
 
-        fun makeBufferooEntityList(count: Int): List<BufferooEntity> {
-            val bufferooEntities = mutableListOf<BufferooEntity>()
+        fun makeDataItemEntityList(count: Int): List<DataItemEntity> {
+            val dataItemEntities = mutableListOf<DataItemEntity>()
             repeat(count) {
-                bufferooEntities.add(makeBufferooEntity())
+                dataItemEntities.add(makeDataItemEntity())
             }
-            return bufferooEntities
+            return dataItemEntities
         }
 
-        fun makeBufferooList(count: Int): List<Bufferoo> {
-            val bufferoos = mutableListOf<Bufferoo>()
+        fun makeDataItemList(count: Int): List<DataItem> {
+            val dataItems = mutableListOf<DataItem>()
             repeat(count) {
-                bufferoos.add(makeBufferoo())
+                dataItems.add(makeDataItem())
             }
-            return bufferoos
+            return dataItems
+        }
+
+        fun makeWeekEntity(): WeekEntity {
+            return WeekEntity(
+                    data = makeDataItemEntityList(2),
+                    change = randomDouble()
+            )
+        }
+
+        fun makeWeek(): Week {
+            return Week(
+                    data = makeDataItemList(2),
+                    change = randomDouble()
+            )
+        }
+
+        fun makeMonthEntity(): MonthEntity {
+            return MonthEntity(
+                    data = makeDataItemEntityList(2),
+                    change = randomDouble()
+            )
+        }
+
+        fun makeMonth(): Month {
+            return Month(
+                    data = makeDataItemList(2),
+                    change = randomDouble()
+            )
+        }
+
+        fun makeAllEntity(): AllEntity {
+            return AllEntity(
+                    data = makeDataItemEntityList(2),
+                    change = randomDouble()
+            )
+        }
+
+        fun makeAll(): All {
+            return All(
+                    data = makeDataItemList(2),
+                    change = randomDouble()
+            )
+        }
+
+        fun makeSeaEntity(): SeaEntity {
+            return SeaEntity(
+                    makeAllEntity(),
+                    makeWeekEntity(),
+                    makeMonthEntity(),
+                    randomInt()
+            )
+        }
+
+        fun makeSea(): Sea {
+            return Sea(
+                    makeAll(),
+                    makeWeek(),
+                    makeMonth(),
+                    randomInt()
+            )
+        }
+
+        fun makeGlobalEntity(): GlobalEntity {
+            return GlobalEntity(
+                    makeAllEntity(),
+                    makeWeekEntity(),
+                    makeMonthEntity(),
+                    randomInt()
+            )
+        }
+
+        fun makeGlobal(): Global {
+            return Global(
+                    makeAll(),
+                    makeWeek(),
+                    makeMonth(),
+                    randomInt()
+            )
+        }
+
+
+        fun makeItemEntity(): ItemEntity {
+            return ItemEntity(
+                    image = randomUuid(),
+                    name = randomUuid(),
+                    global = makeGlobalEntity(),
+                    sea = makeSeaEntity(),
+                    type = randomInt(),
+                    globalSeaDiff = randomDouble()
+            )
+        }
+
+        fun makeItem(): Item {
+            return Item(
+                    image = randomUuid(),
+                    name = randomUuid(),
+                    global = makeGlobal(),
+                    sea = makeSea(),
+                    type = randomInt(),
+                    globalSeaDiff = randomDouble()
+            )
         }
 
     }
