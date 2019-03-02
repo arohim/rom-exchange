7f2173533578642398b13af47e31b827d8ddcd38
Abdulrohim 'Him' Sama <Abdulrohim.S@kaidee.com>
2019-03-02 14:38:24 +0700

    renamed xml file

--- mobile-ui/src/main/java/net/rom/exchange/browse/BrowseAdapter.kt
+++ mobile-ui/src/main/java/net/rom/exchange/browse/BrowseAdapter.kt
@@ -38,7 +38,7 @@ class BrowseAdapter @Inject constructor() : RecyclerView.Adapter<RecyclerView.Vi
             VIEW_ITEM -> {
                 val itemView = LayoutInflater
                         .from(parent.context)
-                        .inflate(R.layout.item_rom_exchange_item, parent, false)
+                        .inflate(R.layout.view_item_exchange_browse, parent, false)
                 return ItemViewHolder(itemView)
             }
             VIEW_PROG -> {
