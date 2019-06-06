e0d60c0b201e4580b1e7e69b8c9595d82cfb03be
Abdulrohim 'Him' Sama <abdulrohim.s@kaidee.com>
2019-06-06 20:17:31 +0700

    clean up

--- mobile-ui/src/main/java/net/rom/exchange/ROMExchangeApplication.kt
+++ mobile-ui/src/main/java/net/rom/exchange/ROMExchangeApplication.kt
@@ -1,19 +1,24 @@
 package net.rom.exchange
 
+import android.app.Activity
 import android.app.Application
 import android.support.v4.app.Fragment
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
 import dagger.android.support.HasSupportFragmentInjector
 import net.danlew.android.joda.JodaTimeAndroid
 import net.rom.exchange.injection.DaggerApplicationComponent
 import timber.log.Timber
 import javax.inject.Inject
 
-class ROMExchangeApplication : Application(), HasSupportFragmentInjector {
+class ROMExchangeApplication : Application(), HasSupportFragmentInjector, HasActivityInjector {
 
     @Inject
     lateinit var activityDispatchingAndroidSupportInjector: DispatchingAndroidInjector<Fragment>
+
+    @Inject
+    lateinit var activityDispatchingAndroidInjector: DispatchingAndroidInjector<Activity>
 
     override fun onCreate() {
         super.onCreate()
@@ -34,6 +39,10 @@ class ROMExchangeApplication : Application(), HasSupportFragmentInjector {
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment> {
         return activityDispatchingAndroidSupportInjector
+    }
+
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return activityDispatchingAndroidInjector
     }
 
 }
